28e188ed76a30ef6b2b98d6a229728d9a6af7c1a
AdRusinek <rusinekadrian19@gmail.com>
2019-07-03 17:52:01 +0200

    Added test for throwing exceptions and handling it

---
+++ src/main/java/guru/springframework/sfgpetclinic/controllers/ValueNotFoundException.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+/**
+ * Created by Adrian Rusinek on 03.07.2019
+ **/
+public class ValueNotFoundException extends RuntimeException {
+
+}
--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,15 +16,18 @@ class IndexControllerTest {
         controller = new IndexController();
     }
 
+    @DisplayName("Test good view name is returned for index page")
     @Test
     void index() {
         assertEquals("index", controller.index());
         assertEquals("index", controller.index(), "Wrong view returned");
     }
 
+    @DisplayName("Test exception")
     @Test
     void oupsHandler() {
-        assertTrue("notimplemented".equals(controller.oupsHandler()),
-                () -> "This is some expensive " + "Message to build ");
+        assertThrows(ValueNotFoundException.class, () -> {
+            controller.oopsHandler();
+        });
     }
 }
